356774999deae0fef3040543cfec375f8e26547a
EJ <115877407+H41s3@users.noreply.github.com>
2024-08-19 22:40:12 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openLink() {
+    // URL with autoplay enabled
+    const url = "https://www.pornhub.com/view_video.php?viewkey=669e9b67930cf";
+    window.open(url, '_blank'); // Opens the link in a new tab for better cross-device compatibility
+}
